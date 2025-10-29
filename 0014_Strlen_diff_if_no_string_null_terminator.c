0ca93cf20f51407e112527aa50a40f21775cd79e
Jimmy_c_start <710821222@qq.com>
2025-10-30 07:08:13 +0800

    If a string has no a string null terminator, length calculated by "strlen" will be difference.

---
+++ 0014_Strlen_diff_if_no_string_null_terminator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	char arr1[] = "abcdefg";
+	char arr2[] = { 'a','b','c','d','e','f','g' };
+	printf("%d\n", strlen(arr1));
+	printf("%d\n", strlen(arr2));
+	return 0;
+}
