335f7080e5b4a0470335ffd049920dceed0dd7d9
Jimmy_c_start <710821222@qq.com>
2025-11-01 07:59:04 +0800

    Add if lines equal to or larger than 2000 to a loop statement.

---
+++ 0021_Loop_statement_add_if_line_equal_or_larger_than_2000.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int line = 0;
+	printf("Joining Fly Mountain.\n");
+	while (line < 2000)
+	{
+		printf("Type a line of code.\n");
+		line++;
+	}
+	if (line >= 2000)
+		printf("Get a decent job.\n");
+	return 0;
+}
