7032c7905c2dade8f82c991a5028a71b01282401
Jimmy_c_start <710821222@qq.com>
2025-11-01 07:53:15 +0800

    The first loop statement.

---
+++ 0020_Loop_statement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int line = 0;
+	printf("Joining Fly Mountain.\n");
+	while (line < 2000)
+	{
+		printf("Type a line of code.\n");
+		line++;
+	}		
+	printf("Get a decent job.\n");
+	return 0;
+}
