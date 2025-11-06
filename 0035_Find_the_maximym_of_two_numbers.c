9c7f9eb315beb31b745a23aa21249600c4d7953f
Jimmy_c_start <710821222@qq.com>
2025-11-06 08:56:05 +0800

    Find out the maximum one of two numbers

---
+++ 0035_Find_the_maximym_of_two_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int num1 = 10;
+	int num2 = 20;
+	if (num1 > num2)
+		printf("The maximum number is: %d\n", num1);
+	else
+		printf("The maximum number is: %d\n", num2);
+	return 0;
+}
