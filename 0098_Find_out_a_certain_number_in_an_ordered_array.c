73b808a2da5e5738ca925c1df01c379f906c328e
Jimmy_c_start <710821222@qq.com>
2025-11-27 06:05:49 +0800

    Find out a certain number in an ordered array

---
+++ 0098_Find_out_a_certain_number_in_an_ordered_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		if (k == arr[i])
+		{
+			printf("Found it, index is: %d\n", i);
+			break;
+		}
+	}
+	if (i == sz)
+		printf("Can not find out.\n");
+	return 0;
+}
