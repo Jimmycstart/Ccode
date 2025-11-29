3ee122ba2ae23620845322df5ec15818d2573c74
Jimmy_c_start <710821222@qq.com>
2025-11-29 15:11:03 +0800

    Judge if two characters are equal to each other with strcmp instead of equal sign.

---
+++ 0104_Judge_if_2_characters_equal_with_strcmp_instead.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i = 0;
+	char password[20] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("Please enter password:>");
+		scanf("%s", password);
+		if (strcmp(password, "123456") == 0)
+		{
+			printf("Sign in success!\n");
+			break;
+		}
+	}
+	if (i == 3)
+		printf("Three times error, exit program.\n");
+	return 0;
+}
