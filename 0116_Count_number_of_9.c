65188ba4b93412e309a9776220cb5d505c5d8fa9
Jimmy_c_start <710821222@qq.com>
2025-12-04 11:38:58 +0800

    Count numbers of digit 9

---
+++ 0116_Count_number_of_9.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<string.h>
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i = 0;
+	int count = 0;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 10 == 9)
+			count++;
+		else if (i / 10 == 9)
+			count++;
+	}
+	printf("count = %d\n", count);
+	return 0;
+}
