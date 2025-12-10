e590d06cabdb04c1e23c7080d2099add5240f2c4
Jimmy_c_start <710821222@qq.com>
2025-12-10 08:39:00 +0800

    Print twinkle characters

---
+++ 0136_Print_twinkle_characters.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<windows.h>
+
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void drawBlock(int x, int y)
+{
+	gotoxy(x, y);
+	printf("\33[32m\33[5m\u25A0\r\n");
+	gotoxy(x, y + 1);
+	printf("\33[31m\33[5m\u2588\r\n");
+	gotoxy(x, y + 2);
+	printf("\33[33m\33[5m\u25A1\r\n");
+	gotoxy(x, y + 3);
+	printf("\33[34m\33[5m\u2593\r\n");
+	gotoxy(x, y + 4);
+	printf("\33[37m\33[5m\u2593\r\n");
+}
+
+int main()
+{
+	int x = 10;
+	for (int i = 0; i < 20; i++)
+	{
+		system("cls");
+		drawBlock(x, 5);
+		x++;
+		Sleep(1600);
+	}
+	printf("\33[37m\33[25m\u2593\r\n");
+	return 0;
+}
