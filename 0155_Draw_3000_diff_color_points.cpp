59d8866be618aa7b73fffd66abefce6bdf933490
Jimmy_c_start <710821222@qq.com>
2025-12-17 08:26:47 +0800

    Draw 3000 different color points

---
+++ 0155_Draw_3000_diff_color_points.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<easyx.h>
+#include<stdio.h>
+int main()
+{
+	initgraph(800, 600);
+	setorigin(400, 300);
+	setaspectratio(1, -1);
+	int x, y;
+	for (int i = 0; i < 1000; i++)
+	{
+		x = rand() % (800 + 1) - 400;
+		y = rand() % (600 + 1) - 300;
+		putpixel(x, y, WHITE);
+	}
+	for (int i = 0; i < 1000; i++)
+	{
+		x = rand() % (800 + 1) - 400;
+		y = rand() % (600 + 1) - 300;
+		putpixel(x, y, RED);
+	}
+	for (int i = 0; i < 1000; i++)
+	{
+		x = rand() % (800 + 1) - 400;
+		y = rand() % (600 + 1) - 300;
+		putpixel(x, y, GREEN);
+	}
+	getchar();
+	closegraph();
+	return 0;
+}
