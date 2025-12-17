fb76d767e2d320231c2a03b8c3e74cc9eb3376dc
Jimmy_c_start <710821222@qq.com>
2025-12-18 07:55:32 +0800

    Draw 3000 points and 150 planes

---
+++ 0158_Draw_3000_points_n_150_planes.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<easyx.h>
+#include<stdio.h>
+int main()
+{
+	initgraph(800, 600);
+	setorigin(400, 300);
+	setaspectratio(1, -1);
+	int x, y;
+	for (int i = 0; i < 1000; i++)
+	{
+		x = rand() % (800 + 1) - 400;
+		y = rand() % (600 + 1) - 300;
+		putpixel(x, y, WHITE);
+	}
+	for (int i = 0; i < 1000; i++)
+	{
+		x = rand() % (800 + 1) - 400;
+		y = rand() % (600 + 1) - 300;
+		putpixel(x, y, RED);
+	}
+	for (int i = 0; i < 1000; i++)
+	{
+		x = rand() % (800 + 1) - 400;
+		y = rand() % (600 + 1) - 300;
+		putpixel(x, y, GREEN);
+	}
+	for (int i = 0; i < 150; i++)
+	{
+		x = rand() % (800 + 1) - 400;
+		y = rand() % (600 + 1) - 300;
+		outtextxy(x, y, L"\u2708");
+	}
+	getchar();
+	closegraph();
+	return 0;
+}
