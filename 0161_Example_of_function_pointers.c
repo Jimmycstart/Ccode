c107e5c642727b4a34bb38ee9d59de501446489a
Jimmy_c_start <710821222@qq.com>
2025-12-19 06:07:26 +0800

    Example of function pointers

---
+++ 0161_Example_of_function_pointers.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int Add(int x, int y)
+{
+	return x + y;
+}
+int main()
+{
+	int (*pAdd)(int, int) = Add;
+	int sum = (*pAdd)(1, 2);
+	printf("sum = %d\n", sum);
+	return 0;
+}
