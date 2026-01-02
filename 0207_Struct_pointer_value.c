579c56a9eb9b68a020d40ce2c893be83db6db827
Jimmy_c_start <710821222@qq.com>
2026-01-03 07:35:07 +0800

    Struct pointer value

---
+++ 0207_Struct_pointer_value.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//由于还没学习结构体，这里告知结构体的大小是20个字节
+struct Test
+{
+	int Num;
+	char* pcName;
+	short sDate;
+	char cha[2];
+	short sBa[4];
+}*p;
+//假设p的值为0x100000，如下表达式的值分别为多少？
+//已知结构体Test类型的变量大小是20个字节。
+int main()
+{
+	p = (struct Test*)0x100000;
+	printf("%p\n", p + 0x1);
+	printf("%p\n", (unsigned long)p + 0x1);
+	printf("%p\n", (unsigned int*)p + 0x1);
+	return 0;
+}
