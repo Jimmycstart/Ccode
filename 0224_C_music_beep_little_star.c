96f0849b54902e7a60c5ac6b198154af6dc3b328
Jimmy_c_start <710821222@qq.com>
2026-01-09 07:57:33 +0800

    C music beep little star

---
+++ 0224_C_music_beep_little_star.c
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+int main()
+{
+	printf("小星星\n\n");
+
+	printf("一闪一闪  ");
+	Beep(523, 375);
+	Beep(523, 375);
+	Beep(784, 375);
+	Beep(784, 375);
+
+	printf("亮晶晶\n\n");
+	Beep(880, 375);
+	Beep(880, 375);
+	Beep(784, 750);
+
+	printf("满天都是  ");
+	Beep(698, 375);
+	Beep(698, 375);
+	Beep(659, 375);
+	Beep(659, 375);
+
+	printf("小星星\n\n");
+	Beep(587, 375);
+	Beep(587, 375);
+	Beep(523, 750);
+
+	printf("挂在天空  ");
+	Beep(784, 375);
+	Beep(784, 375);
+	Beep(698, 375);
+	Beep(698, 375);
+
+	printf("放光明\n\n");
+	Beep(659, 375);
+	Beep(659, 375);
+	Beep(587, 750);
+
+	printf("好像许多  ");
+	Beep(784, 375);
+	Beep(784, 375);
+	Beep(698, 375);
+	Beep(698, 375);
+
+	printf("小眼睛\n\n");
+	Beep(659, 375);
+	Beep(659, 375);
+	Beep(587, 750);
+
+	printf("一闪一闪  ");
+	Beep(523, 375);
+	Beep(523, 375);
+	Beep(784, 375);
+	Beep(784, 375);
+
+	printf("亮晶晶\n\n");
+	Beep(880, 375);
+	Beep(880, 375);
+	Beep(784, 750);
+
+	printf("满天都是  ");
+	Beep(698, 375);
+	Beep(698, 375);
+	Beep(659, 375);
+	Beep(659, 375);
+
+	printf("小星星\n\n");
+	Beep(587, 375);
+	Beep(587, 375);
+	Beep(523, 750);
+
+	return 0;
+}
