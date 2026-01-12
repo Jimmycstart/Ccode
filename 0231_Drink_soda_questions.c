ee602fbe0dde8ab68163faf458c5d0e70972957a
Jimmy_c_start <710821222@qq.com>
2026-01-12 08:16:21 +0800

    Drink soda questions

---
+++ 0231_Drink_soda_questions.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int money = 0;
+	int total = 0;
+	int empty = 0;
+	scanf("%d", &money);
+	total = money;
+	empty = money;
+	while (empty >= 2)
+	{
+		total += empty / 2;
+		empty = empty / 2 + empty % 2;
+	}
+	printf("total = &d\n", total);
+	return 0;
+}
