94825d8a07fbdecab73657f4b0ab46a06a6590ae
Jimmy_c_start <710821222@qq.com>
2026-01-14 08:32:34 +0800

    Find n factorial with recursion

---
+++ 0235_Find_n_factorial_with_recursion.c
@@ -0,0 +1,25 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int Fac2(int n)
+{
+	int i = 0;
+	if (n <= 1)
+		return 1;
+	else
+		return n * Fac2(n - 1);
+	int ret = 1;
+	for (i = 1; i <= n; i++)
+	{
+		ret *= i;
+	}
+	return ret;
+}
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret = Fac2(n);
+	printf("%d\n", ret);
+	return 0;
+}
