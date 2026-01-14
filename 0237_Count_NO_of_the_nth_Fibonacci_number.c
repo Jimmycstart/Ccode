c1dc059193fa2d90b2d4c8cd305a92177476d747
Jimmy_c_start <710821222@qq.com>
2026-01-14 08:35:36 +0800

    Count number of the nth Fibonacci number

---
+++ 0237_Count_NO_of_the_nth_Fibonacci_number.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int count = 0;
+int Fib(int n)
+{
+	if (n == 3)
+	{
+		count++;
+	}
+	if (n <= 2)
+		return 1;
+	else
+		return Fib(n - 1) + Fib(n - 2);
+}
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret = Fib(n);
+	printf("ret = %d\n", ret);
+	printf("count = %d\n", count);
+	return 0;
+}
