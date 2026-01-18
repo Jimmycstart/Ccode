88ce76fbafd3dd770f2324febf40594883d2a644
Jimmy_c_start <710821222@qq.com>
2026-01-18 08:30:40 +0800

    Print all char array element assigned first

---
+++ 0248_Print_all_char_array_element_assigned_first.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	char arr[] = "abcdef";
+	int i = 0;
+	int len = strlen(arr);
+	for (i = 0; i < len; i++)
+	{
+		printf("%c", arr[i]);
+	}
+	return 0;
+}
