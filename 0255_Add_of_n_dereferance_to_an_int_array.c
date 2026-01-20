9e1ad45024dfc459e95a5f23d383476426cf8e0f
Jimmy_c_start <710821222@qq.com>
2026-01-20 08:40:47 +0800

    Address of and dereference to an int array

---
+++ 0255_Add_of_n_dereferance_to_an_int_array.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a[5] = { 5, 4, 3, 2, 1 };
+	int* ptr = (int*)(&a + 1);
+	printf("%d, %d", *(a + 1), *(ptr - 1));
+	return 0;
+}
