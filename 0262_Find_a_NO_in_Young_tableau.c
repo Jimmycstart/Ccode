447256088ba1d098c901da8368e56f8db4584c27
Jimmy_c_start <710821222@qq.com>
2026-01-23 09:25:24 +0800

    Find a number in Young tableau

---
+++ 0262_Find_a_NO_in_Young_tableau.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int FindNum(int arr[3][3], int k, int row, int col)
+{
+	int x = 0;
+	int y = col - 1;
+	while (x <= row - 1 && y >= 0)
+	{
+		if (arr[x][y] > k)
+		{
+			y--;
+		}
+		else if (arr[x][y] < k)
+		{
+			x++;
+		}
+		else
+		{
+			printf("Index is: %d %d\n", x, y);
+			return 1;
+		}
+	}
+	return 0;
+}
+int main()
+{
+	int arr[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
+	int k = 7;
+	int ret = FindNum(arr, k, 3, 3);
+	if (ret == 1)
+	{
+		printf("Found it.\n");
+	}
+	else
+	{
+		printf("Not find.\n");
+	}
+	return 0;
+}
