8cc232abc6ea5b8ead2ecbcbef2446df225e34e5
Jimmy_c_start <710821222@qq.com>
2026-01-24 08:56:56 +0800

    Can strlen be negative or not

---
+++ 0266_Can_strlen_be_negative_or_not.c
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	if ((strlen("abc") - strlen("abcdef")) > 0)
+	{
+		printf("hehe\n");
+	}
+	else
+	{
+		printf("haha\n");
+	}
+	return 0;
+}
