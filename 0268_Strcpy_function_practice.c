c3aac26d440a9067a2ad5f22585730f7e836e1f5
Jimmy_c_start <710821222@qq.com>
2026-01-25 09:16:31 +0800

    Strcpy function practice

---
+++ 0268_Strcpy_function_practice.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+my_strcpy(char* dest, char* src)
+{
+	assert(dest != NULL);
+	assert(src != NULL);
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = *src;
+}
+int main()
+{
+	char arr1[] = "abcdefghi";
+	char arr2[] = "bit";
+	my_strcpy(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
