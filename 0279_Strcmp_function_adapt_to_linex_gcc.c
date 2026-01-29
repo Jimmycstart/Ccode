608cabf5b4ee0e1e49815b33c564a6ded6e26420
Jimmy_c_start <710821222@qq.com>
2026-01-29 08:21:41 +0800

    Strcmp function adapt to linux_gcc

---
+++ 0279_Strcmp_function_adapt_to_linex_gcc.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+int my_strcmp(const char* str1, const char* str2)
+{
+	assert(str1 && str2);
+	while (*str1 == *str2)
+	{
+		if (*str1 == '\0')
+		{
+			return 0;
+		}
+		str1++;
+		str2++;
+	}
+	return (*str1 - *str2);
+}
+int main()
+{
+	char* p1 = "abcdef";
+	char* p2 = "abqwe";
+	int ret = my_strcmp(p1, p2);
+	printf("ret = %d\n", ret);
+	return 0;
+}
