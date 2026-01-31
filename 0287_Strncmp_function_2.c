2032470aa47858e7017d42cb1a645d65436d1686
Jimmy_c_start <710821222@qq.com>
2026-02-01 06:26:44 +0800

    Strncmp function 2

---
+++ 0287_Strncmp_function_2.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	const char* p1 = "abcdef";
+	const char* p2 = "abcqwer";
+	int ret = strncmp(p1, p2, 4);
+	printf("%d\n", ret);
+	return 0;
+}
