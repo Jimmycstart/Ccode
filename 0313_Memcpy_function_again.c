19be563d9cedd84646be1189170e9ed1e9d1b4a0
Jimmy_c_start <710821222@qq.com>
2026-02-09 06:44:34 +0800

    Memcpy function again

---
+++ 0313_Memcpy_function_again.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int arr1[] = { 1, 2, 3, 4, 5 };
+	int arr2[10] = { 0 };
+	memcpy(arr2, arr1, sizeof(arr1));
+	return 0;
+}
