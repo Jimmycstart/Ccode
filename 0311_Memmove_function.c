7b1edecc508ec2e59b80ac2dca99987df21eba05
Jimmy_c_start <710821222@qq.com>
2026-02-09 06:41:33 +0800

    Memmove function

---
+++ 0311_Memmove_function.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int i = 0;
+	memmove(arr + 2, arr, 20);
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
