53b028068d1869564081782aeca8fab36feb948d
Jimmy_c_start <710821222@qq.com>
2026-02-17 07:59:31 +0800

    Size of struct

---
+++ 0335_Size_of_struct.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+struct S1
+{
+	char c1;
+	int a;
+	char c2;
+};
+struct S2
+{
+	char c1;
+	char c2;
+	int a;
+};
+int main()
+{
+	struct S1 s1 = { 0 };
+	printf("%d\n", sizeof(s1));
+	struct S2 s2 = { 0 };
+	printf("%d\n", sizeof(s2));
+	return 0;
+}
