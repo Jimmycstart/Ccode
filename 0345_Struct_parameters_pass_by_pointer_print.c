d5676faeb369e6c6cdf552f54964d8932152b087
Jimmy_c_start <710821222@qq.com>
2026-02-20 20:57:07 +0800

    Struct parameters pass by pointer print

---
+++ 0345_Struct_parameters_pass_by_pointer_print.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+struct S
+{
+	int a;
+	char c;
+	double d;
+};
+void Init(struct S* ps)
+{
+	ps->a = 100;
+	ps->c = 'w';
+	ps->d = 3.14;
+}
+void Print1(struct S tmp)
+{
+	printf("%d %c %lf\n", tmp.a, tmp.c, tmp.d);
+}
+int main()
+{
+	struct S s = { 0 };
+	Init(&s);
+	Print1(s);
+	return 0;
+}
