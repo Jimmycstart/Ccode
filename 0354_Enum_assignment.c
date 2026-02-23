633dc78dc33ab5522af3b20036dd61d4659f16e0
Jimmy_c_start <710821222@qq.com>
2026-02-23 19:33:34 +0800

    Enum assignment

---
+++ 0354_Enum_assignment.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+enum Sex
+{
+	MALE = 2,
+	FEMALE = 4,
+	SECRET = 8
+};
+enum Color
+{
+	RED,
+	YELLOW,
+	BLUE
+};
+int main()
+{
+	enum Color c = BLUE;
+	printf("%d %d %d\n", RED, YELLOW, BLUE);
+	printf("%d %d %d\n", MALE, FEMALE, SECRET);
+	return 0;
+}
