3f3ff58d87ef2442d1ed410ae10964bfb2b96fea
Jimmy_c_start <710821222@qq.com>
2026-02-23 19:35:20 +0800

    Enum assignment but not variable

---
+++ 0355_Enum_assignment_but_not_variable.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+enum Sex
+{
+	MALE = 2,
+	FEMALE = 4,
+	SECRET = 8
+};
+enum Color
+{
+	RED,
+	YELLOW,
+	BLUE
+};
+int main()
+{
+	enum Color c = BLUE;
+	MALE = 0;
+	printf("%d %d %d\n", RED, YELLOW, BLUE);
+	printf("%d %d %d\n", MALE, FEMALE, SECRET);
+	return 0;
+}
