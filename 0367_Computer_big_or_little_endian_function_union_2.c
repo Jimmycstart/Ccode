59868344531bcc310dc6a27e630724b3f60677c0
Jimmy_c_start <710821222@qq.com>
2026-02-27 17:41:08 +0800

    Computer big or little endian function union 2

---
+++ 0367_Computer_big_or_little_endian_function_union_2.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int check_sys()
+{
+	union
+	{
+		char c;
+		int i;
+	}u;
+	u.i = 1;
+	return u.c;
+}
+int main()
+{
+	int a = 1;
+	int ret = check_sys();
+	if (1 == ret)
+	{
+		printf("Little endian\n");
+	}
+	else
+	{
+		printf("Big endian\n");
+	}
+	return 0;
+}
