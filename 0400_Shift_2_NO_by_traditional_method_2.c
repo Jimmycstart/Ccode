2b76fe03674652c8de1b38f63e1edc0e235c4659
Jimmy_c_start <710821222@qq.com>
2026-03-11 07:56:05 +0800

    Shift two numbers by traditional method 2

---
+++ 0400_Shift_2_NO_by_traditional_method_2.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	int b = 5;
+
+	printf("Before shift: a = %d b = %d\n", a, b);
+
+	a = a + b;
+	b = a - b;
+	a = a - b;
+
+	printf("After shift: a = %d b = %d\n", a, b);
+
+	return 0;
+}
