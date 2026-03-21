c98082bf8c997d0f1215a8b28cf7eabd0f3beeda
Jimmy_c_start <710821222@qq.com>
2026-03-21 18:21:09 +0800

    Address of Struct STU

---
+++ 0430_Address_of_Struct_STU.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+struct Stu
+{
+	char name[20];
+	int age;
+	char id[20];
+};
+
+int main()
+{
+	struct Stu s1 = { "ZHANGSAN", 20, "2019010305"};
+	struct Stu* ps = &s1;
+	printf("%s\n", (*ps).name);
+	printf("%d\n", (*ps).age);
+	printf("%s\n", (*ps).id);
+	return 0;
+}
