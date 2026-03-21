6d8159c45a2844f50b5b1562fc3a3179d1386f5b
Jimmy_c_start <710821222@qq.com>
2026-03-21 18:19:51 +0800

    Comma expression 2

---
+++ 0428_Comma_expression_2.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	if (a = b + 1, c = a / 2, d > 0)
+	{
+		printf("Hehe\n");
+	}
+	else
+		printf("Haha\n");
+	return 0;
+}
