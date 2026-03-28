27063d8614d7a957d8515e3fd24df76e77d8cd2f
Jimmy_c_start <710821222@qq.com>
2026-03-28 17:35:40 +0800

    Pointer out of bounds access

---
+++ 0451_Pointer_out_of_bounds_access.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	int* p = arr;
+	int i = 0;
+	for (i = 0; i <= 12; i++)
+	{
+		*(p++) = i;
+	}
+	return 0;
+}
