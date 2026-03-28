7b958151deff638efd437e0202c442bae9e2aef9
Jimmy_c_start <710821222@qq.com>
2026-03-28 17:31:03 +0800

    Put char pointer into arr address

---
+++ 0449_Put_char_pointer_into_arr_add.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	char* p = (char*)arr;			//◊¢“‚º”…œ(char*)
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		*(p + i) = 1;
+	}
+	return 0;
+}
