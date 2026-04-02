bfffc271789f2acbff126ffcd9b10b76394f5cf9
Jimmy_c_start <710821222@qq.com>
2026-04-02 22:44:44 +0800

    Pointer minus pointer 2

---
+++ 0461_Pointer_minus_pointer_2.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	char ch[5] = { 0 };
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	printf("%d\n", &arr[9] - &arr[0]);
+	printf("%d\n", &arr[9] - &ch[0]);
+	return 0;
+}
