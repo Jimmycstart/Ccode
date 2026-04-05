4fef681f154f364bf9925f1e697a4df53d682828
Jimmy_c_start <710821222@qq.com>
2026-04-05 21:38:12 +0800

    Array name is address of first element 3

---
+++ 0467_Array_name_is_add_of_first_element_3.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	printf("%p\n", arr);
+	printf("%p\n", arr);
+	printf("%p\n", &arr[0]);
+	printf("%p\n", &arr[0] + 1);
+	printf("%p\n", &arr);
+	printf("%p\n", &arr + 1);
+	return 0;
+}
