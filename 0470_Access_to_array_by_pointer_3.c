7c269b454a1e133f89a575c2c05da50d052cd1b7
Jimmy_c_start <710821222@qq.com>
2026-04-07 21:58:53 +0800

    Access to array by pointer 3

---
+++ 0470_Access_to_array_by_pointer_3.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,0 };
+	int* p = arr;
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		*(p + 1) = i;
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
