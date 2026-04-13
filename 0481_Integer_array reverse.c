68060ae1eb8940711aff6489aaf868d9f890d267
Jimmy_c_start <710821222@qq.com>
2026-04-13 21:35:04 +0800

    Integer array reverse

---
+++ 0481_Integer_array reverse.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void Reverse(int arr[], int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left < right)
+	{
+		int tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	Reverse(arr, sz);
+	return 0;
+}
