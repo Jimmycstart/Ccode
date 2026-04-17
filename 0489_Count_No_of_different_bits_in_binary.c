768464e33e68e479468c095c50e9613fa0ad1039
Jimmy_c_start <710821222@qq.com>
2026-04-17 21:19:39 +0800

    Count number of different bits in binary

---
+++ 0489_Count_No_of_different_bits_in_binary.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int get_diff_bit(int m, int n)
+{
+	int tmp = m ^ n;
+}
+int main()
+{
+	int m = 0;
+	int n = 0;
+	scanf("%d%d", &m, &n);
+	int count = get_diff_bit(m, n);
+	printf("count = %d\n", count);
+	return 0;
+}
