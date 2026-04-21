51fe6cc33bb7da3f7be58b859deddc91dda35f4c
Jimmy_c_start <710821222@qq.com>
2026-04-21 21:32:48 +0800

    Count numbers of 1 in binary 4

---
+++ 0495_Count_No_of_1_in_binary_4.c
@@ -0,0 +1,1 @@
+#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>int count_bit_one(int n){	int count = 0;	int i = 0;	for (i = 0; i < 32; i++)	{		if (((n >> i) & 1) == 1)		{			count++;		}	}	return count;}int main(){	int a = 0;	scanf("%d", &a);	int count = count_bit_one(a);	printf("count = %d\n", count);	system("pause");	return 0;}
