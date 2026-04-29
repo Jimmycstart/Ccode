5ded77aff7cc42765c91b7ef8eedabf348f52c4b
Jimmy Zhao <710821222@qq.com>
2026-04-29 21:18:01 +0800

    Update 0511_Passing_parameters_in_structure_2.c

--- 0511_Passing_parameters_in_structure_2.c
+++ 0511_Passing_parameters_in_structure_2.c
@@ -1,1 +1,33 @@
-#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>typedef struct Stu{	char name[20];	short age;	char tele[12];	char sex[5];}Stu;void Print1(Stu tmp){	printf("name: %s\n", tmp.name);	printf("age: %d\n", tmp.age);	printf("tele: %s\n", tmp.tele);	printf("sex: %s\n", tmp.sex);}void Print2(Stu* ps){	printf("name: %s\n", ps->name);	printf("age: %d\n", ps->age);	printf("tele: %s\n", ps->tele);	printf("sex: %s\n", ps->sex);}int main(){	Stu s = { "Lisi", 40, "15598886688", "Male"};	Print1(s);	Print2(&s);	return 0;}
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+typedef struct Stu
+{
+	char name[20];
+	short age;
+	char tele[12];
+	char sex[5];
+}Stu;
+
+void Print1(Stu tmp)
+{
+	printf("name: %s\n", tmp.name);
+	printf("age: %d\n", tmp.age);
+	printf("tele: %s\n", tmp.tele);
+	printf("sex: %s\n", tmp.sex);
+}
+
+void Print2(Stu* ps)
+{
+	printf("name: %s\n", ps->name);
+	printf("age: %d\n", ps->age);
+	printf("tele: %s\n", ps->tele);
+	printf("sex: %s\n", ps->sex);
+}
+
+int main()
+{
+	Stu s = { "Lisi", 40, "15598886688", "Male"};
+	Print1(s);
+	Print2(&s);
+	return 0;
+}
