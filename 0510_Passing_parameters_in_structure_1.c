d4ceafc2380e87cd8ee7ac06f940be0eaa261ae1
Jimmy_c_start <710821222@qq.com>
2026-04-29 21:04:36 +0800

    Passing parameters in structure 1

---
+++ 0510_Passing_parameters_in_structure_1.c
@@ -0,0 +1,1 @@
+#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>typedef struct Stu{	char name[20];	short age;	char tele[12];	char sex[5];}Stu;void Print1(Stu tmp){	printf("name: %s\n", tmp.name);	printf("age: %d\n", tmp.age);	printf("tele: %s\n", tmp.tele);	printf("sex: %s\n", tmp.sex);}int main(){	Stu s = { "Lisi", 40, "15598886688", "Male"};	Print1(s);	return 0;}
