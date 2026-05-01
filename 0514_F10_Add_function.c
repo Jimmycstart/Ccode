15b5410e514e86b6fee03be45811fc2ffe68ac47
Jimmy_c_start <710821222@qq.com>
2026-05-01 23:41:01 +0800

    F10 add function

---
+++ 0514_F10_Add_function.c
@@ -0,0 +1,1 @@
+#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>int Add(int x, int y){	return x + y;}int main(){	printf("hehe\n");	int a = 20;	int b = 10;	int c = Add(a, b);	return 0;}
