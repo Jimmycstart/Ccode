adb35a706c80a093e8f357d2760294647f29ab79
Jimmy_c_start <710821222@qq.com>
2026-05-05 18:29:34 +0800

    Good coding example strcpy

---
+++ 0522_Good_coding_example_strcpy.c
@@ -0,0 +1,1 @@
+#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>int main(){	char arr1[] = "##################";	char arr2[] = "bit";	strcpy(arr1, arr2);	printf("%s\n", arr1);	printf("%s\n", arr2);	return 0;}
