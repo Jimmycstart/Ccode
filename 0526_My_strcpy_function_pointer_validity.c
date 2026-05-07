98bdc86fdba9e0ed2a52b7926e8f3f0c19804d60
Jimmy_c_start <710821222@qq.com>
2026-05-07 21:10:04 +0800

    My strcpy  function pointer validity

---
+++ 0526_My_strcpy_function_pointer_validity.c
@@ -0,0 +1,1 @@
+#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>void my_strcpy(char* dest, char* src){	if (dest != NULL && src != NULL)	{		while (*dest++ = *src++)		{			;		}	}}int main(){	char arr1[] = "##################";	char arr2[] = "bit";	my_strcpy(arr1, NULL);	printf("%s\n", arr1);	printf("%s\n", arr2);	return 0;}
