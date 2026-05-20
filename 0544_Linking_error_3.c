f850cba5c1ab534bb701858b7437baa6556656c2
Jimmy_c_start <710821222@qq.com>
2026-05-20 17:48:43 +0800

    Linking error 3

---
+++ 0544_Linking_error_3.c
@@ -0,0 +1,1 @@
+#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>int Add(int x, int y){	return x + y;}int main(){	int a = 10;	int b = 20;	int sum = Add(a, b);	printf("%d\n", sum);	return 0;}
