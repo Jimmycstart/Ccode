048c96fa7c2e3e6870f27c9ab5f3ceea8ef30848
Jimmy_c_start <710821222@qq.com>
2026-05-22 14:58:46 +0800

    Check system function large or small end

---
+++ 0548_Check_system_function_large_or_small_end.c
@@ -0,0 +1,1 @@
+#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>check_sys(){	int a = 1;	char* p = (char*)&a;	if (*p == 1)	{		return 1;	}	else		return 0;}int main(){	int ret = check_sys();	if (ret == 1)	{		printf("Small end\n");	}	else	{		printf("Large end\n");	}	return 0;}
