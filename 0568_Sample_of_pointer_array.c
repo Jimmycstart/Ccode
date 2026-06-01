e4b5856481afb1134acff800d39a0ffbe400d023
Jimmy_c_start <710821222@qq.com>
2026-06-01 16:37:27 +0800

    Sample of pointer array

---
+++ 0568_Sample_of_pointer_array.c
@@ -0,0 +1,1 @@
+#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>int main(){	int a = 10;	int b = 20;	int c = 30;	int d = 40;	int* arr[4] = { &a, &b, &c, &d };	int i = 0;	for (i = 0; i < 4; i++)	{		printf("%d\n", *(arr[i]));	}	return 0;}
