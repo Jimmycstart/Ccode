0edd4d4b540f36b58902c667fc246f6ba90b6940
Jimmy_c_start <710821222@qq.com>
2026-06-11 23:11:07 +0800

    1D array argument passing

---
+++ 0586_1D_array_argument_passing.c
@@ -0,0 +1,1 @@
+#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>void test(int arr[]){}void test(int arr[10]){}void test(int* arr){}void test2(int* arr2[20]){}void test2(int** arr){}int main(){	int arr[10] = { 0 };	int* arr2[20] = { 0 };	test(arr);	test2(arr2);	return 0;}
