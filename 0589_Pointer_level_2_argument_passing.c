53b2a848d86c7b0c2b195c631cd6b90da5fb74b4
Jimmy_c_start <710821222@qq.com>
2026-06-13 22:10:58 +0800

    Pointer level 2 argument passing

---
+++ 0589_Pointer_level_2_argument_passing.c
@@ -0,0 +1,1 @@
+#define _CRT_SECURE_NO_WARNINGS 1#include<stdio.h>void test(int** ptr){	printf("num = %d\n", **ptr);}int main(){	int n = 10;	int* p = &n;	int** pp = &p;	test(pp);	test(&p);	return 0;}
